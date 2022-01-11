138e3c7e0704a82f78fb0ce4a764294552c532ed
Tom van Wijgerden <tom.vanwijgerden@visma.com>
2022-01-11 14:12:48 +0100

    fix;

--- src/app/routes/public/login/login.component.ts
+++ src/app/routes/public/login/login.component.ts
@@ -16,14 +16,14 @@ export class LoginComponent implements OnInit {
   isLoggedIn = false;
   isLoginFailed = false;
   errorMessage = '';
-  roles: string[] = [];
+  name: string[] = [];
 
   constructor(private authService: AuthService, private tokenStorage: TokenStorageService) { }
 
   ngOnInit(): void {
     if (this.tokenStorage.getToken()) {
       this.isLoggedIn = true;
-      this.roles = this.tokenStorage.getUser().roles;
+      this.name = this.tokenStorage.getUser().name;
     }
   }
 
@@ -37,7 +37,7 @@ export class LoginComponent implements OnInit {
 
         this.isLoginFailed = false;
         this.isLoggedIn = true;
-        this.roles = this.tokenStorage.getUser().roles;
+        this.name = this.tokenStorage.getUser().name;
         this.reloadPage();
       },
       error: (err: { error: { message: string; }; }) => {
