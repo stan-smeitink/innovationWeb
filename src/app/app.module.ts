13f3acb3167716923ee3c35156154849c3f4496f
Stan Smeitink <stan.smeitink@visma.com>
2022-01-11 18:30:50 +0100

    clean project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
 import { CoreModule } from './core/core.module';
 import { LayoutModule } from './layout/layout.module';
-import { SharedModule } from './shared/shared.module';
+import { SharedModule } from "./shared/shared.module";
 import { RoutingModule } from './routes/routing.module';
-import { FormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,8 +20,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     LayoutModule,
     SharedModule.forRoot(),
     RoutingModule,
-    FormsModule,
-    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,30 @@
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+// https://angular.io/styleguide#!#04-10
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  declarations: [],
+  exports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+  ],
+})
+
+// https://github.com/ocombe/ng2-translate/issues/209
+export class SharedModule {
+  static forRoot(): ModuleWithProviders<SharedModule> {
+    return {
+      ngModule: SharedModule,
+    };
+  }
+}
