3d581758a716f2166ae8583da5d27d0b0b9581e5
Tom van Wijgerden <tom.vanwijgerden@visma.com>
2022-01-12 13:17:26 +0100

    new routes + views

--- src/app/routes/public/public.module.ts
+++ src/app/routes/public/public.module.ts
@@ -6,11 +6,17 @@ import {ReactiveFormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
 import {RegisterComponent} from './register/register.component';
 import {FormsModule} from '@angular/forms'; //TODO: replace with SharedModule?
+import { DossiersComponent } from './dossiers/dossiers.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { EmployersComponent } from './employers/employers.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent}
+  {path: 'register', component: RegisterComponent},
+  {path: 'employers', component: EmployersComponent},
+  {path: 'employees', component: EmployeesComponent},
+  {path: 'dossiers', component: DossiersComponent},
 ];
 
 @NgModule({
@@ -20,7 +26,8 @@ const routes: Routes = [
     CommonModule,
     FormsModule
   ],
-  declarations: [HomeComponent, LoginComponent, RegisterComponent],
+  declarations: [HomeComponent, LoginComponent, RegisterComponent, DossiersComponent, EmployeesComponent, EmployersComponent],
   exports: [RouterModule]
 })
+
 export class PublicModule { }
