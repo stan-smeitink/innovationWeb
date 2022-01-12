b36f11614aec828681d528b527e63582ae4a1aff
Stan Smeitink <stan.smeitink@visma.com>
2022-01-12 17:07:04 +0100

    removed some spaces

--- src/app/core/helpers/options.interceptor.ts
+++ src/app/core/helpers/options.interceptor.ts
@@ -4,8 +4,6 @@ import { HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http'
 
 import { Observable } from 'rxjs';
 
-
-
 @Injectable()
 export class OptionsInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
