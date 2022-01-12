85025a7067d0850e16221dcd721c99e272495be5
Tom van Wijgerden <tom.vanwijgerden@visma.com>
2022-01-12 15:00:12 +0100

    made employees table

--- src/app/routes/secure/employees/employees.component.ts
+++ src/app/routes/secure/employees/employees.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {EmployersService} from "../../../services/employers.service";
+import {EmployeesService} from "../../../services/employees.service";
 
 @Component({
   selector: 'app-employees',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employees.component.scss']
 })
 export class EmployeesComponent implements OnInit {
-
-  constructor() { }
+  public employeesArray = [];
+  constructor(private employees:EmployeesService) { }
 
   ngOnInit(): void {
+    this.employees.all().subscribe({
+      next: (result: { employers: any;}) => {
+        console.log(result);
+        this.employeesArray = result['data'];
+      },
+      error: (err: { error: { message: string; }; }) => {
+
+      }
+    });
+  }
+
+  public show(id){
+
+  }
+
+  public delete(id){
+    this.employees.delete(id).subscribe({
+      next: (result: { employers: any;}) => {
+        this.ngOnInit();
+      },
+      error: (err: { error: { message: string; }; }) => {
+        console.log("Error");
+      }
+    });
   }
 
 }
