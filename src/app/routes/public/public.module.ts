81e831bb09a77448d290224503b400027f2a1096
Tom van Wijgerden <tom.vanwijgerden@visma.com>
2022-01-12 13:18:59 +0100

    new routes + views

--- src/app/routes/public/public.module.ts
+++ src/app/routes/public/public.module.ts
@@ -6,17 +6,11 @@ import {ReactiveFormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
 import {RegisterComponent} from './register/register.component';
 import {FormsModule} from '@angular/forms'; //TODO: replace with SharedModule?
-import { DossiersComponent } from './dossiers/dossiers.component';
-import { EmployeesComponent } from './employees/employees.component';
-import { EmployersComponent } from './employers/employers.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'employers', component: EmployersComponent},
-  {path: 'employees', component: EmployeesComponent},
-  {path: 'dossiers', component: DossiersComponent},
 ];
 
 @NgModule({
@@ -26,7 +20,7 @@ const routes: Routes = [
     CommonModule,
     FormsModule
   ],
-  declarations: [HomeComponent, LoginComponent, RegisterComponent, DossiersComponent, EmployeesComponent, EmployersComponent],
+  declarations: [HomeComponent, LoginComponent, RegisterComponent],
   exports: [RouterModule]
 })
 
--- src/app/routes/secure/secure.module.ts
+++ src/app/routes/secure/secure.module.ts
@@ -2,14 +2,20 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {SecureComponent} from './secure/secure.component';
 import {CommonModule} from '@angular/common';
+import {EmployersComponent} from "./employers/employers.component";
+import {EmployeesComponent} from "./employees/employees.component";
+import {DossiersComponent} from "./dossiers/dossiers.component";
 
 const routes: Routes = [
   {path: 'secure', component: SecureComponent},
+  {path: 'employers', component: EmployersComponent},
+  {path: 'employees', component: EmployeesComponent},
+  {path: 'dossiers', component: DossiersComponent},
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes), CommonModule],
-  declarations: [SecureComponent],
+  declarations: [SecureComponent, DossiersComponent, EmployeesComponent, EmployersComponent],
   exports: [RouterModule]
 })
 
