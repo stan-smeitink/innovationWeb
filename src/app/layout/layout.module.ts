094017533b769a14ba6af2bc070aae478ff1ce3b
Tom van Wijgerden <tom.vanwijgerden@visma.com>
2022-01-12 11:23:10 +0100

    login route

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,18 +3,18 @@ import {NgModule} from '@angular/core';
 import {LayoutComponent} from './layout.component';
 import {SidebarComponent} from './sidebar/sidebar.component';
 import {HeaderComponent} from './header/header.component';
-import {AuthService} from '../core/auth/auth.service';
 
 import {FooterComponent} from './footer/footer.component';
 
 import {SharedModule} from '../shared/shared.module';
+import {TokenStorageService} from "../core/auth/token-storage.service";
 
 @NgModule({
   imports: [
     SharedModule
   ],
   providers: [
-    AuthService
+    TokenStorageService
   ],
   declarations: [
     LayoutComponent,
